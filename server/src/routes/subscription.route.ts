8d23d45a583b8889a3b2601beffa084cb85405ce
dilshan.d3 <dilshan72404@gmail.com>
2025-12-30 21:56:19 +0530

    Add subscription routes for managing user subscriptions

--- server/src/routes/subscription.route.ts
+++ server/src/routes/subscription.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { purchaseSubscription } from "../controllers/subscription.controller";
+import { cancelUserSubscription, getMySubscriptions, purchaseSubscription, updateSubscription } from "../controllers/subscription.controller";
 import { VerifyToken } from "../middlewares/verifyToken";
 const router = Router();
 
 router.route('/purchaseSubscription').post(VerifyToken, purchaseSubscription);
+router.route('/getMySubscriptions').get(VerifyToken, getMySubscriptions);
+router.route('/cancelUserSubscription/:id').get(VerifyToken, cancelUserSubscription);
+router.route('/updateSubscription').post(VerifyToken, updateSubscription);
 
 export default router;
