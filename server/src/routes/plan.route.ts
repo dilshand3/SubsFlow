7916b093cf1601c6375d44238297fb249a4f7b07
dilshan.d3 <dilshan72404@gmail.com>
2025-12-30 21:57:04 +0530

    Add admin verification to plan management routes

--- server/src/routes/plan.route.ts
+++ server/src/routes/plan.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { VerifyToken } from "../middlewares/verifyToken";
-import { createPlan, getAllActivePlans } from "../controllers/plan.controller";
+import { createPlan, deletePlanDetail, editPlanDetail, getAllActivePlans } from "../controllers/plan.controller";
+import { verifyAdmin } from "../middlewares/AdminVerfiy";
 const router = Router();
 
-router.route('/createPlan').post(VerifyToken, createPlan);
-router.route('/getAllActivePlan').get(getAllActivePlans)
+router.route('/createPlan').post(VerifyToken, verifyAdmin, createPlan);
+router.route('/getAllActivePlan').get(getAllActivePlans);
+router.route('/editPlanDetail/:id').post(VerifyToken, verifyAdmin, editPlanDetail);
+router.route('/deletePlanDetail/:id').get(VerifyToken, verifyAdmin, deletePlanDetail);
 
 export default router;
